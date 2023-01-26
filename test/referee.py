c0bbf2d9e95bb975ce616d5f5566a5e1095f136e
Danmirror <danuandreanid@gmail.com>
2023-01-26 23:24:26 +0700

    add new test for referee

--- test/referee.py
+++ test/referee.py
@@ -8,7 +8,10 @@ import socket
 
 localIP     = "0.0.0.0"
 localPort   = 3838
-sendPort    = 3939
+
+oldSendPort = 3838
+newSendPort = 3939
+
 bufferSize  = 1024
 
 # Create a datagram socket
@@ -21,8 +24,8 @@ print("UDP server up and listening")
 while(True):
     bytesAddressPair,peer = UDPServerSocket.recvfrom(bufferSize)
 
-    clientMsg = "Message from Client:{}".format(bytesAddressPair[9])
+    # clientMsg = "Message from Client:{}".format(bytesAddressPair[9])
     
-    print(clientMsg)
-    destination = peer[0], sendPort
-    UDPServerSocket.sendto(b'RGrt\x02\x01\x02', destination)
+    print(bytesAddressPair)
+    destination = peer[0], oldSendPort
+    UDPServerSocket.sendto(b'RGrt\x02\x07\x01', destination)
