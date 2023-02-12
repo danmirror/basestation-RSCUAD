52307b279cc4144b60b6529c8d6a906442d54453
Danmirror <danuandreanid@gmail.com>
2023-02-12 23:36:53 +0700

    add 5 robot cam and fixed error invalid memory

--- server.go
+++ server.go
@@ -11,13 +11,22 @@ import (
 func main() {
 	go manager.RefereeBoxHandler()
 	go manager.ClientHandler()
-	go manager.StreamHandler()
+	go manager.StreamHandlerR1()
+	go manager.StreamHandlerR2()
+	go manager.StreamHandlerR3()
+	go manager.StreamHandlerR4()
+	go manager.StreamHandlerR5()
 	manager.Init()
 
 	styles := http.FileServer(http.Dir("./assets/"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", styles))
 	http.HandleFunc("/ws", manager.WSHandler)
-	http.HandleFunc("/video", manager.VideoHandler)
+	http.HandleFunc("/video1", manager.VideoHandlerR1)
+	http.HandleFunc("/video2", manager.VideoHandlerR2)
+	http.HandleFunc("/video3", manager.VideoHandlerR3)
+	http.HandleFunc("/video4", manager.VideoHandlerR4)
+	http.HandleFunc("/video5", manager.VideoHandlerR5)
+
 	http.HandleFunc("/", RootHandler)
 
 	panic(http.ListenAndServe(":8081", nil))
--- source/stream.go
+++ source/stream.go
@@ -11,13 +11,24 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var videoImage image.Image
+var videoImageR1 image.Image
+var videoImageR2 image.Image
+var videoImageR3 image.Image
+var videoImageR4 image.Image
+var videoImageR5 image.Image
+
+var portR1 = 8131
+var portR2 = 8132
+var portR3 = 8133
+var portR4 = 8134
+var portR5 = 8135
+
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
 
-func VideoHandler(w http.ResponseWriter, r *http.Request) {
+func VideoHandlerR1(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println(err)
@@ -28,19 +39,114 @@ func VideoHandler(w http.ResponseWriter, r *http.Request) {
 
 	for {
 		jpegBuf := &bytes.Buffer{}
-		jpeg.Encode(jpegBuf, videoImage, nil)
-		err = conn.WriteMessage(websocket.BinaryMessage, jpegBuf.Bytes())
-		if err != nil {
-			fmt.Println(err)
-			return
+
+		if videoImageR1 != nil {
+			jpeg.Encode(jpegBuf, videoImageR1, nil)
+			err = conn.WriteMessage(websocket.BinaryMessage, jpegBuf.Bytes())
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
 		}
 	}
 }
 
-func StreamHandler() {
+func VideoHandlerR2(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		jpegBuf := &bytes.Buffer{}
+
+		if videoImageR2 != nil {
+			jpeg.Encode(jpegBuf, videoImageR2, nil)
+			err = conn.WriteMessage(websocket.BinaryMessage, jpegBuf.Bytes())
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+	}
+}
+
+func VideoHandlerR3(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		jpegBuf := &bytes.Buffer{}
+
+		if videoImageR3 != nil {
+			jpeg.Encode(jpegBuf, videoImageR3, nil)
+			err = conn.WriteMessage(websocket.BinaryMessage, jpegBuf.Bytes())
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+	}
+}
+
+func VideoHandlerR4(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		jpegBuf := &bytes.Buffer{}
+
+		if videoImageR4 != nil {
+			jpeg.Encode(jpegBuf, videoImageR4, nil)
+			err = conn.WriteMessage(websocket.BinaryMessage, jpegBuf.Bytes())
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+	}
+}
+
+func VideoHandlerR5(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		jpegBuf := &bytes.Buffer{}
+
+		if videoImageR5 != nil {
+			jpeg.Encode(jpegBuf, videoImageR5, nil)
+			err = conn.WriteMessage(websocket.BinaryMessage, jpegBuf.Bytes())
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+	}
+}
+
+func StreamHandlerR1() {
 	// Listen for incoming video data on the specified address and port
 	addr := net.UDPAddr{
-		Port: 8125,
+		Port: portR1,
 		IP:   net.ParseIP(GetIP()),
 	}
 	conn, _ := net.ListenUDP("udp", &addr)
@@ -53,6 +159,86 @@ func StreamHandler() {
 	for {
 		// Read the incoming video data
 		n, _, _ := conn.ReadFromUDP(buf)
-		videoImage, _, _ = image.Decode(bytes.NewReader(buf[:n]))
+		videoImageR1, _, _ = image.Decode(bytes.NewReader(buf[:n]))
+	}
+}
+
+func StreamHandlerR2() {
+	// Listen for incoming video data on the specified address and port
+	addr := net.UDPAddr{
+		Port: portR2,
+		IP:   net.ParseIP(GetIP()),
+	}
+	conn, _ := net.ListenUDP("udp", &addr)
+	defer conn.Close()
+
+	// Create an image buffer to store the incoming video data
+	buf := make([]byte, 65507)
+
+	// Continuously receive and decode the video data
+	for {
+		// Read the incoming video data
+		n, _, _ := conn.ReadFromUDP(buf)
+		videoImageR2, _, _ = image.Decode(bytes.NewReader(buf[:n]))
+	}
+}
+
+func StreamHandlerR3() {
+	// Listen for incoming video data on the specified address and port
+	addr := net.UDPAddr{
+		Port: portR3,
+		IP:   net.ParseIP(GetIP()),
+	}
+	conn, _ := net.ListenUDP("udp", &addr)
+	defer conn.Close()
+
+	// Create an image buffer to store the incoming video data
+	buf := make([]byte, 65507)
+
+	// Continuously receive and decode the video data
+	for {
+		// Read the incoming video data
+		n, _, _ := conn.ReadFromUDP(buf)
+		videoImageR3, _, _ = image.Decode(bytes.NewReader(buf[:n]))
+	}
+}
+
+func StreamHandlerR4() {
+	// Listen for incoming video data on the specified address and port
+	addr := net.UDPAddr{
+		Port: portR4,
+		IP:   net.ParseIP(GetIP()),
+	}
+	conn, _ := net.ListenUDP("udp", &addr)
+	defer conn.Close()
+
+	// Create an image buffer to store the incoming video data
+	buf := make([]byte, 65507)
+
+	// Continuously receive and decode the video data
+	for {
+		// Read the incoming video data
+		n, _, _ := conn.ReadFromUDP(buf)
+		videoImageR4, _, _ = image.Decode(bytes.NewReader(buf[:n]))
+	}
+}
+
+func StreamHandlerR5() {
+	// Listen for incoming video data on the specified address and port
+	addr := net.UDPAddr{
+		Port: portR5,
+		IP:   net.ParseIP(GetIP()),
+	}
+	conn, _ := net.ListenUDP("udp", &addr)
+	defer conn.Close()
+
+	// Create an image buffer to store the incoming video data
+	buf := make([]byte, 65507)
+
+	// Continuously receive and decode the video data
+	for {
+		// Read the incoming video data
+		n, _, _ := conn.ReadFromUDP(buf)
+		videoImageR5, _, _ = image.Decode(bytes.NewReader(buf[:n]))
 	}
 }
